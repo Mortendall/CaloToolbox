0055a90eba31eef76386faa28c5f86c72d8ba8cb
Morten Dall <mortendall1988@gmail.com>
2026-07-08 13:22:12 +0200

    perf: changed layout of welcome page

--- R/mod_welcome.R
+++ R/mod_welcome.R
@@ -11,60 +11,61 @@ mod_welcome_ui <- function(id) {
   ns <- NS(id)
   tagList(
     bslib::layout_columns(
-      col_widths = 4,
-        shiny::column(12),
+      col_widths = 12,
+        #shiny::column(12),
         shiny::column(12,
                       bslib::card(
                         bslib::card_header("Welcome to the CaloToolbox"),
                         bslib::card_body(
-                          bslib::card_body(
+                          bslib::card_body(shiny::tagList(
                             htmltools::span(
                               "A collection of tools for further processing indirect calorimetry data processed with ",
                               shiny::a("CalR", href = "https://calrapp.org/")
-                            )
+                            ),
+                            bslib::layout_columns(
+                              col_widths = 4,
+                              shiny::column(12,
+                                            bslib::card(
+                                              bslib::card_header("The Outlier Tool"),
+                                              bslib::card_body("Detect outliers and inspect data",
+                                                               shinyWidgets::actionBttn(
+                                                                 inputId = ns("outlier"),
+                                                                 style = "jelly",
+                                                                 label = "launch outlier tool",size = "s",
+                                                                 block = F
+                                                               ))
+
+                                            ),
+                                            bslib::card(
+                                              bslib::card_header("Manual Food Joiner"),
+                                              bslib::card_body("Add manually weighed daily food intake to your dataset")
+                                            )
+                              ),
+                              shiny::column(12,
+                                            bslib::card(
+                                              bslib::card_header("Trim drifting sensor"),
+                                              bslib::card_body("")
+                                            )
+                                            #,
+                                            #not really necessary as this can be done with the macro
+                                            # bslib::card(
+                                            #   bslib::card_header("CalR paster"),
+                                            #   bslib::card_body("Paste together CalR runs that were started
+                                            #                    and stopped, and zero cumulative values")
+                                            # )
+                              ),
+                              shiny::column(12,
+                                            bslib::card(
+                                              bslib::card_header("EMM Multiple Comparison Tester"),
+                                              bslib::card_body("Perform a multiple comparison test of
+                                       Estimated Marginal Means")
+                                            ))
+                            ))
                           )
 
                         )
                       ))),
-    bslib::layout_columns(
-      col_widths = 4,
-      shiny::column(12,
-                    bslib::card(
-                      bslib::card_header("The Outlier Tool"),
-                      bslib::card_body("Detect outliers and inspect data",
-                                       shinyWidgets::actionBttn(
-                                         inputId = ns("outlier"),
-                                         style = "jelly",
-                                         label = "launch outlier tool",size = "s",
-                                         block = F
-                                       ))
-
-                    ),
-                    bslib::card(
-                      bslib::card_header("Manual Food Joiner"),
-                      bslib::card_body("Add manually weighed daily food intake to your dataset")
-                    )
-                    ),
-      shiny::column(12,
-                    bslib::card(
-                      bslib::card_header("Trim drifting sensor"),
-                      bslib::card_body("")
-                    )
-                    #,
-                    #not really necessary as this can be done with the macro
-                    # bslib::card(
-                    #   bslib::card_header("CalR paster"),
-                    #   bslib::card_body("Paste together CalR runs that were started
-                    #                    and stopped, and zero cumulative values")
-                    # )
-                    ),
-      shiny::column(12,
-                    bslib::card(
-                      bslib::card_header("EMM Multiple Comparison Tester"),
-                      bslib::card_body("Perform a multiple comparison test of
-                                       Estimated Marginal Means")
-                    ))
-    ))
+    )
 
 }
 
